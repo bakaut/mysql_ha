8d30da138c67914d497a913548191688f0891227
bakaut <fifo.mail@gmail.com>
2018-11-23 23:35:18 +0300

    Update recreate.sh

--- opt/recreate.sh
+++ opt/recreate.sh
@@ -5,22 +5,16 @@ rm -rf /var/lib/mysql/*
 > /var/log/mysqld.log
 systemctl start mysqld
 pass=`tail -10000 /var/log/mysqld.log | grep "temporary password" | awk '{print $13}'`
-echo $pass
 
-query="SET sql_log_bin=OFF;set global super_read_only=OFF;ALTER USER 'root'@'localhost' IDENTIFIED BY '7,Agf5Cupp0E,';update mysql.user set Host='%' where user='root';flush privileges;set global super_read_only=ON;SET sql_log_bin=ON"
+cat << EOF | mysql --connect-expired-password -p"${pass}"
+SET sql_log_bin=OFF;
+set global super_read_only=OFF;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '7,Agf5Cupp0E,';
+update mysql.user set Host='%' where user='root';
+flush privileges;
+set global super_read_only=ON;
+SET sql_log_bin=ON;
+EOF
 
-echo "${query}"
-mysql --connect-expired-password -p"${pass}" -e "${query}"
 systemctl restart mysqld
 
-
-#check it
-#cat << EOF | psql ---params
-#BEGIN;
-#
-#`pg_dump ----something`
-#
-#update table .... statement ...;
-#
-#END;
-#EOF
